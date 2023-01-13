0841eb42922976d2c9c08abf3933be2b084059ee
Morgan <morgand@tutamail.com>
2023-01-13 19:07:16 +0100

    init commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import CreateEmployee from './pages/CreateEmployee/CreateEmployee'
+import EmployeeList from './pages/EmployeeList/EmployeeList';
+// import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  //<React.StrictMode>
+  <Router>
+    <Routes>
+      <Route path="/" element={<CreateEmployee />} />
+      <Route path="/list" element={<EmployeeList />} />
+    </Routes>
+  </Router>
+  //</React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
