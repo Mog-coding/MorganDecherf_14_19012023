7ba57ee1051abf16729fca35e822d0992dbbb303
Morgan <morgand@tutamail.com>
2023-01-25 18:11:49 +0100

    add Home component

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import './Home.css';
+
+
+export default function Home() {
+
+    return (
+        <main className="home__main">
+            <article className="home__article">
+                <img src="/logo.png" alt='logo_wealth' className='home__logo' />
+                <p>Welcome to Wealth Health HRnet</p>
+            </article>
+        </main>
+    )
+}
