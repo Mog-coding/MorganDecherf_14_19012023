138b76ffa5ba12b48d0ebe7e4b2321482851127c
Morgan <morgand@tutamail.com>
2023-01-28 11:02:51 +0100

    add states picker with data modelisation

---
+++ src/model/statesModel.js
@@ -0,0 +1,12 @@
+export class StatesModel {
+    constructor(dataJS) {
+      if (dataJS) {
+        this.statesData = dataJS.map((el) => {
+          return {
+            value: el.name,
+            label: el.name,
+          }
+        })
+      }
+    }
+  }
--- src/pages/CreateEmployee/CreateEmployee.js
+++ src/pages/CreateEmployee/CreateEmployee.js
@@ -2,8 +2,14 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { saisie } from '../../actions/adressActions';
 import './CreateEmployee.css';
+//react-datepicker
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+//react-select
+import Select from 'react-select';
+//data
+import { StatesModel } from '../../model/statesModel';
+import { statesData } from '../../utils/statesData.js';
 
 export default function CreateEmployeeclass() {
     const dispatch = useDispatch();
@@ -11,10 +17,15 @@ export default function CreateEmployeeclass() {
     const [birthDate, setBirthDate] = useState('');
     const [startDate, setStartDate] = useState('');
 
+    const [selectedOption, setSelectedOption] = useState(null);
+
     const handleSubmit = (e) => {
         e.preventDefault();
         dispatch(saisie(firstName));
     };
+
+  const test = new StatesModel(statesData);
+  const options = test.statesData;
 
     return (
         <main className="create">
@@ -22,7 +33,9 @@ export default function CreateEmployeeclass() {
             <form className="create__form" onSubmit={(e) => handleSubmit(e)}>
                 <div className="create__info">
                     <h2>Employee informations</h2>
-                    <label htmlFor="first-name" className="create__label">First Name</label>
+                    <label htmlFor="first-name" className="create__label">
+                        First Name
+                    </label>
                     <input
                         type="text"
                         id="first-name"
@@ -31,7 +44,9 @@ export default function CreateEmployeeclass() {
                             setFirstName(e.target.value);
                         }}
                     />
-                    <label htmlFor="last-name" className="create__label">Last Name</label>
+                    <label htmlFor="last-name" className="create__label">
+                        Last Name
+                    </label>
                     <input
                         type="text"
                         id="last-name"
@@ -43,18 +58,22 @@ export default function CreateEmployeeclass() {
                             selected={birthDate}
                             onChange={(date) => setBirthDate(date)}
                             className="datePickerCustom"
-                            placeholderText={"Click to select a date"}
+                            placeholderText={'Click to select a date'}
                         />
                     </div>
-                    <label htmlFor="start-date" className="create__label">Start Date</label>
+                    <label htmlFor="start-date" className="create__label">
+                        Start Date
+                    </label>
                     <DatePicker
-                            selected={startDate}
-                            onChange={(date) => setStartDate(date)}
-                            className="datePickerCustom"
-                            placeholderText={"Click to select a date"}
-                        />
+                        selected={startDate}
+                        onChange={(date) => setStartDate(date)}
+                        className="datePickerCustom"
+                        placeholderText={'Click to select a date'}
+                    />
                     <div className="create__info__department">
-                        <label htmlFor="department" className="create__label">Department</label>
+                        <label htmlFor="department" className="create__label">
+                            Department
+                        </label>
                         <select name="department" id="department">
                             <option>Sales</option>
                             <option>Marketing</option>
@@ -67,25 +86,64 @@ export default function CreateEmployeeclass() {
 
                 <div className="create__address">
                     <h2>Employee address</h2>
-                    <label htmlFor="street" className="create__label">Street</label>
-                    <input
-                        id="street"
-                        type="text"
-                        className="create__input"
+                    <label htmlFor="street" className="create__label">
+                        Street
+                    </label>
+                    <input id="street" type="text" className="create__input" />
+                    <label htmlFor="city" className="create__label">
+                        City
+                    </label>
+                    <input id="city" type="text" className="create__input" />
+                    <label htmlFor="state" className="create__label">
+                        State
+                    </label>
+                    <Select
+                        defaultValue={selectedOption}
+                        onChange={setSelectedOption}
+                        options={options}
+                        placeholder= 'Click to select a state'
+                        menuPosition='fixed'
+                        menuPlacement='auto'
+                        styles={{
+                            control: (baseStyles, state) => ({
+                                ...baseStyles,
+                                minHeight: '25px',
+                                fontSize: '14px',
+                                marginBottom: '20px',
+                                borderRadius: '5px;',
+                                "&:hover": {
+                                    border: "2px solid grey",
+                                    boxShadow: 'none',
+                                    outline: 'none',
+                                },
+                                border: state.isFocused ? '2px solid black' : '2px solid rgb(235, 235, 235)',
+                                boxShadow: state.isFocused ? "" : "",
+                            }),
+                            dropdownIndicator: (base) => ({
+                                ...base,
+                                paddingTop: 0,
+                                paddingBottom: 0,
+                            }),
+                            valueContainer: (base) => ({
+                                ...base,
+                                padding: 0,
+                            }),
+                            menuList: (base) => ({
+                                ...base,
+                                fontSize: '12px',
+                            }),
+                            option: (provided, state) => ({
+                                ...provided,
+                                backgroundColor: state.isSelected ? '#586F07' : 'white',
+                                "&:hover": {
+                                    backgroundColor: '#CEDA97',
+                                }
+                            }),
+                        }}
                     />
-                    <label htmlFor="city" className="create__label">City</label>
-                    <input
-                        id="city"
-                        type="text"
-                        className="create__input"
-                    />
-                    <label htmlFor="state" className="create__label">State</label>
-                    <select
-                        name="state"
-                        id="state"
-                        className="create__input"
-                    ></select>
-                    <label htmlFor="zip-code" className="create__label">Zip Code</label>
+                    <label htmlFor="zip-code" className="create__label">
+                        Zip Code
+                    </label>
                     <input
                         id="zip-code"
                         type="number"
