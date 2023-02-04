bf4633929178358abc145cdacde714418ec58010
Morgan <morgand@tutamail.com>
2023-02-04 11:01:24 +0100

    add states abbreviations in data

--- src/model/statesModel.js
+++ src/model/statesModel.js
@@ -1,12 +1,8 @@
-export class StatesModel {
-    constructor(dataJS) {
-        if (dataJS) {
-            this.statesData = dataJS.map((el) => {
-                return {
-                    value: el.name,
-                    label: el.name,
-                }
-            })
+export function statesModel(statesDataJS) {
+    return statesDataJS.map((el) => {
+        return {
+            value: el.abbreviation,
+            label: el.name,
         }
-    }
+    })
 }
