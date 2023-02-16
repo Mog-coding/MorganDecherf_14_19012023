d47451b4c8c10e605e3108eea8e5f9c17600f686
Morgan <morgand@tutamail.com>
2023-02-16 12:00:54 +0100

    fix bug table

--- src/pages/EmployeeList/EmployeeList.js
+++ src/pages/EmployeeList/EmployeeList.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import "./EmployeeList.css";
+import './EmployeeList.css';
 import { Table } from '../../components/Table/Table.js';
-import { useSelector } from "react-redux";
+import { useSelector } from 'react-redux';
 import { dateFormat } from '../../utils/dateFormat';
 
 export default function EmployeeList() {
-
-    const employeesList = useSelector(state => state.employees);
+    const employeesList = useSelector((state) => state.employees);
 
     const reduxFormat = (employeeList) => {
-        console.log("redux employeeList: ", employeeList)
+        console.log('redux employeeList: ', employeeList);
         return employeeList.map((el) => {
             return {
                 col1: el.firstName,
                 col2: el.lastName,
-                col3: dateFormat(el.startDate),
-                col4: el.selectedDepartment.value,
-                col5: dateFormat(el.birthDate),
+                col3: el.startDate ? dateFormat(el.startDate) : '',
+                col4: el.selectedDepartment ? el.selectedDepartment.value : '',
+                col5: el.birthDate ? dateFormat(el.birthDate) : '',
                 col6: el.street,
                 col7: el.city,
-                col8: el.selectedState.value,
+                col8: el.selectedState ? el.selectedState.value : '',
                 col9: el.zipCode,
             };
         });
@@ -67,17 +66,15 @@ export default function EmployeeList() {
         []
     );
 
-    //data -> employeeMockModel(employeeListMock) OU employeesList
     const data = React.useMemo(
         () => reduxFormat(employeesList),
-        []
+        [employeesList]
     );
 
     return (
         <main className="employeeList">
             <h1>Employees list</h1>
-            <button></button>
             <Table columns={columns} data={data} />
         </main>
-    )
+    );
 }
